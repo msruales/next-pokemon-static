66ff78f2e4b1842b3af7eee1ba09c3f1fc8216d1
Marcos Ruales <msruales.94@gmail.com>
2023-01-26 20:44:06 -0500

    nabvar updated

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,4 +1,4 @@
-import {Navbar, Text, useTheme} from '@nextui-org/react';
+import {Container, Navbar, Text, useTheme} from '@nextui-org/react';
 import React from 'react';
 import Image from 'next/image';
 import NextLink from 'next/link';
@@ -8,24 +8,19 @@ export const CustomNavbar = () => {
     return (
         <Navbar isBordered={isDark} variant="sticky">
             <Navbar.Brand>
+                <Image
+                    src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
+                    alt="icono de la app"
+                    width={70}
+                    height={70}
+                />
                 <NextLink href="/" passHref>
-                    <>
-                        <Image
-                            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
-                            alt="icono de la app"
-                            width={70}
-                            height={70}
-                        />
-                        <>
-                            <Text b h2 color="inherit" hideIn="xs">
-                                P
-                            </Text>
-                            <Text b h3 color="inherit" hideIn="xs">
-                                okemon
-                            </Text>
-                        </>
-                    </>
-
+                    <Text b h2 color="inherit" css={{display: 'inline'}} hideIn="xs">
+                        P
+                    </Text>
+                    <Text b h3 color="inherit" css={{display: 'inline'}} hideIn="xs">
+                        okemon!
+                    </Text>
                 </NextLink>
             </Navbar.Brand>
             <Navbar.Content>
